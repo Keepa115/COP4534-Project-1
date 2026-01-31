53198eaf611599447d55587e313fd820d20d8645
Keepa115 <jab325@students.uwf.edu>
2026-01-30 20:27:45 -0600

    added Node.hpp

---
+++ Node.hpp
@@ -0,0 +1,29 @@
+/***************************************************************
+  Student Name: Jerome Ball
+  File Name: Node.hpp
+  Assignment number: Project 1
+
+  This file holds the Node class which will contain the userid as well as
+  encrypted passwords created by the VigenereCipher class.
+***************************************************************/
+
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    string userid;
+    string encryptedPassword;
+    Node* next;
+
+    Node(const string& userid, const string& encryptedPassword){
+        this->userid = userid;
+        this->encryptedPassword = encryptedPassword;
+        this->next = nullptr;
+    }
+};
+
+#endif // NODE_HPP
