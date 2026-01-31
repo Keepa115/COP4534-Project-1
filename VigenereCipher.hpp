0bbea6a7b426dea5b2bbe1659ac929d4b1e96795
Keepa115 <jab325@students.uwf.edu>
2026-01-30 21:27:03 -0600

    fixed error in comment

--- VigenereCipher.hpp
+++ VigenereCipher.hpp
@@ -3,7 +3,7 @@
   File Name: VigenereCipher.hpp
   Assignment number: Project 1
 
-  This file holds the VigenereCipher class which will be used to encrypt and passwords.
+  This file holds the VigenereCipher class which will be used to encrypt passwords.
 
   All functions are declared but are implemented in VigenereCipher.cpp.
 ***************************************************************/
